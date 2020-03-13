6a906b2a13651c60a124a096f503f017b9947641
Lara K.L <lara.kopunovic.legetin@hotmail.com>
2020-03-13 20:25:12 +0100

    Added the project

---
+++ OnOffSwitch/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using OnOffSwitch.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnOffSwitch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thickness originalMargin;
+        OnOffData onOffData = new OnOffData(false);
+
+        public MainWindow()
+        {
+            
+            this.DataContext = onOffData;
+
+            InitializeComponent();
+            
+            originalMargin = OnOffUserControl.Margin;
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if(e.PreviousSize.Height != 0 && e.PreviousSize.Width != 0)
+            {
+                double originalHeight = e.PreviousSize.Height;
+                double originalWidth = e.PreviousSize.Width;
+                double newHeight = ActualHeight;
+                double newWidth = ActualWidth;
+
+                double percentH = newHeight / originalHeight;
+                double percentW = newWidth / originalWidth;
+
+                double controlHeight = OnOffUserControl.Height;
+                double controlWidth = OnOffUserControl.Width;
+
+                OnOffUserControl.Height = controlHeight * percentH;
+                OnOffUserControl.Width = controlWidth * percentW;
+
+                Thickness newMargin = new Thickness();
+                newMargin.Left = originalMargin.Left * percentW;
+                newMargin.Top = originalMargin.Top * percentH;
+                newMargin.Bottom = originalMargin.Bottom * percentH;
+                newMargin.Right = originalMargin.Right * percentW;
+                OnOffUserControl.Margin = newMargin;
+                originalMargin = newMargin;
+            }
+        }
+    }
+}
---
+++ OnOffSwitch/OnOffUserControl.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OnOffSwitch
+{
+    /// <summary>
+    /// Interaction logic for OnOffUserControl.xaml
+    /// </summary>
+    public partial class OnOffUserControl : UserControl
+    {
+        public Boolean IsOn
+        {
+            get { return (Boolean)GetValue(IsOnProperty); }
+            set { SetValue(IsOnProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOn.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOnProperty =
+            DependencyProperty.Register("IsOn", typeof(Boolean), typeof(OnOffUserControl), new UIPropertyMetadata(false));
+
+
+        public OnOffUserControl()
+        {
+            InitializeComponent();
+
+            if (IsOn == true)
+            {
+                OffButton.Background = Brushes.Red;
+                OffButton.Content = "OFF";
+            }
+            else
+            {
+                OnButton.Background = Brushes.Green;
+                OnButton.Content = "ON";
+            }
+        }
+
+        private void On_Click(object sender, RoutedEventArgs e)
+        {
+            IsOn = true;
+            OffButton.Background = Brushes.Red;
+            OnButton.Background = Brushes.Gray;
+            if(ActualHeight < 48 && ActualWidth < 48)
+            {
+                lessThan48();
+            }
+            else
+            {
+                OffButton.Content = "OFF";
+                OnButton.Content = "";
+            }
+            
+        }
+
+        private void lessThan48()
+        {
+            OffButton.Content = "";
+            OnButton.Content = "";
+        }
+
+        private void Off_Click(object sender, RoutedEventArgs e)
+        {
+            IsOn = false;
+            OnButton.Background = Brushes.Green;
+            OffButton.Background = Brushes.Gray;
+            if(ActualWidth < 48 && ActualHeight < 48)
+            {
+                lessThan48();
+            }
+            else
+            {
+                OnButton.Content = "ON";
+                OffButton.Content = "";
+            }
+            
+        }
+
+        //private void off()
+        //{
+
+        //}
+
+        private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (ActualWidth < 48 && ActualHeight < 48)
+            {
+                lessThan48();
+            }
+        }
+    }
+}
